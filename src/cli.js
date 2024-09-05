a49e659607258d7c528637ba07248cc36b1b9c3b
xuqingkai <xuqingkai@hd123.com>
2024-09-05 11:36:55 +0800

    test

--- src/cli.js
+++ src/cli.js
@@ -546,11 +546,12 @@ function updateAndroidMetaData() {
   content = content.replace(/(versionCode\s+)(.*)/, '$1' + $G.manifest.uapp.versionCode)
   content = content.replace(/(versionName\s+")(.*)(")/, '$1' + $G.manifest.uapp.versionName + '$3')
   content = content.replace(/("DCLOUD_APPKEY"\s+:\s+")(.*)(",)/, '$1' + $G.manifest.uapp.appkey + '$3')
+  console.log($G.manifest.uapp, '$G.manifest.uapp');
 
-  content = content.replace(/keyAlias '.*'/, "keyAlias '" +  $G.manifest.uapp.keyAlias + "'")
-  .replace(/keyPassword '.*'/, "keyPassword '" + $G.manifest.uapp.KeyPassword + "'")
-  .replace(/storeFile file\('.*'\)/, "storeFile file('" + $G.manifest.uapp.storeFile + "')")
-  .replace(/storePassword '.*'/, "storePassword '" + $G.manifest.uapp.KeyPassword + "'");
+  content = content.replace(/keyAlias '.*'/, "keyAlias '" + $G.manifest.uapp.keyAlias + "'")
+    .replace(/keyPassword '.*'/, "keyPassword '" + $G.manifest.uapp.keyPassword + "'")
+    .replace(/storeFile file\('.*'\)/, "storeFile file('" + $G.manifest.uapp.storeFile + "')")
+    .replace(/storePassword '.*'/, "storePassword '" + $G.manifest.uapp.keyPassword + "'");
 
   content = content.replace(
     /("WX_APPID"\s+:\s+")(.*)(",)/,
